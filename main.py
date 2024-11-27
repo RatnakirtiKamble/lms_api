a892b7d78d370d3881a1c21a838adf2691d09c49
diogg0 <ratnakirti1234@gmail.com>
2024-11-27 23:04:16 +0530

    flask

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from utils.core_utils import attempt_login, get_subjects, get_subject_materials, get_download_link
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return get_subjects(attempt_login("rat.kam.rt22@dypatil.edu", "Ratna@1234"))
+
+
+@app.route('/get_subjects')
+def get_subjects_route():
+    subjects = get_subjects(attempt_login("rat.kam.rt22@dypatil.edu", "Ratna@1234"))
+
+    return subjects
+    
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
