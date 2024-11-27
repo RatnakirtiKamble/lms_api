3d6c8513af6324c93fafd7cd827d644f4a307f81
Diogg0 <ratnakirti1234@gmail.com>
2024-11-27 22:45:27 +0530

    new

---
+++ utils/core_utils.py
@@ -0,0 +1,181 @@
+import requests
+from bs4 import BeautifulSoup
+from utils.site_parsing import parse_subjects, parse_attendance, parse_materials, parse_resource_link
+
+def attempt_login(username:str, password:str, session = None):
+    """
+        Creates a requests.session, makes a POST request to the LMS site, upon success, it writes
+        the session cookies to the flask session object, to be used for future requests
+
+            Parameters:
+                username (str): A string of the student's official username
+                password (str): A string of the student's password
+
+            Returns:
+                cookie_dict (dict): A dictionary containing the session cookies, as obtained by the Official site
+    """
+    with requests.session() as session:
+
+        USER_DATA = {
+            'username':username,
+            'password':password
+        }
+        response = session.post(
+            url="https://mydy.dypatil.edu/rait/login/index.php",
+            data=USER_DATA,
+            #stream=True  #set it to stream || to test ; non stream version seems to respond faster, need to investigate
+        )
+
+        #response.close() #instantly close the response, as we dont need the 80,000+ character response that contains irrelevant data (html, inline css, and javascript)
+
+        # print(response.content) 
+        #Successful login
+        if response.status_code == 200:
+
+            if response.headers['Content-Type'] == "text/html; charset=utf-8":
+
+
+                #convert to a dictionary
+                cookie_dict = requests.utils.dict_from_cookiejar(session.cookies)
+
+                # return session.cookies, on a successful login
+                return cookie_dict
+
+            else:
+                #incorrect credentials
+                return None
+
+        #if login fails, return the response code
+        return response.status_code
+
+
+def get_subjects(cookies) -> list[dict]:
+
+    """
+        Creates a session object, attaches the login cookies to this instance, and makes the fetch request for the subjects
+
+        Parameters:
+            cookies (dict): Cookies generated upon login action done on the official site
+
+        Returns:
+            subjects (list[dict]): Array of JSON Object/Python Dictionary containing subject details for logged in user:\n
+                \t- Name
+                \t- Instructor
+                \t- Attendance
+                \t- Link
+                \t- Course ID
+    """
+
+    with requests.session() as session:
+
+        #Insert the session cookies
+        session.cookies = requests.utils.cookiejar_from_dict(cookie_dict=cookies)
+        response = session.get('https://mydy.dypatil.edu/rait/blocks/academic_status/ajax.php?action=myclasses')
+
+        html_content = response.content
+        #pass the html_content to a custom parsing block, that converts it into a neat json object 
+        subjects = parse_subjects(html=html_content)
+        # print(subjects)
+
+        return subjects
+
+
+def get_subject_materials(link : str , cookies) -> list[dict]:
+    """
+        Fetches the given link, and returns a JSON containing links to the resource and titles
+
+
+        Parameters:
+            link (str): The subject link to be fetched and processed
+            cookies (dict): Cookies generated upon login action done on the official site
+
+        Returns:
+            materials_json (list[dict]): List of course materials for a particular subject
+    """
+
+    with requests.session() as session:
+        session.cookies = requests.utils.cookiejar_from_dict(cookie_dict=cookies)
+
+        response = session.get(link)
+        #get the string version of the response to pass to the parser
+        response = response.content
+
+        materials_json = PARSE.parse_materials(response)
+
+
+        return materials_json
+
+
+def get_download_link(link : str, link_type, cookies):
+    """
+        Finds and returns a link to the .pdf, .pptx, or .docx resource for easy downloading.
+
+        Parameters:
+            link (str): The subject link to be fetched and processed
+            cookies (dict): Cookies generated upon login action done on the official site
+
+        Returns:
+            An Object containing the following
+            name: name of the file 
+            link: link to the file requested
+    """
+
+    # TODO: Implement a check to determine the kind of link (3-4 types exist I believe)
+    with requests.session() as session:
+
+        session.cookies = requests.utils.cookiejar_from_dict(cookie_dict=cookies)
+
+        response = session.get(link)
+
+        #parsing the response to extract the link
+
+        extracted_link = PARSE.parse_resource_link(response.content, link_type)
+
+        content = None
+        #Return object
+        if (extracted_link != None):
+
+            start = extracted_link.rfind('/') + 1 #get the last "/" and ahead of it, is the file name
+            name = extracted_link[start:]
+            content = {
+                'name': name,
+                'link': extracted_link
+            }
+
+
+
+        return content
+
+
+
+def get_attendance_summary(cookies) -> list[dict]:
+    """
+        Fetches the link, parses and organises the data into a neat JSON array.
+
+        Parameters:
+            link: link to be fetched
+            cookies: session data to persist
+
+        Returns:
+            An Array of JSON Objects with the following attributes  
+
+            - subject : subject name
+            - total : total no. of classes
+            - present : no. of classes attended
+            - absent : no. of classes missed
+            - percentage: ratio of present/absent 
+    """
+
+
+    with requests.session() as session:
+
+        #Insert the session cookies
+        session.cookies = requests.utils.cookiejar_from_dict(cookie_dict=cookies)
+        response = session.get(SITE.ATTENDANCE_URL)
+        response = response.content
+
+        attendance_data = PARSE.parse_attendance(response) 
+
+        if len(attendance_data) == 0:
+            return None       
+        return attendance_data
---
+++ utils/site_parsing.py
@@ -0,0 +1,249 @@
+from bs4 import BeautifulSoup
+import re
+import logging
+
+def parse_subjects(html: str) -> list[dict]:
+    """
+        Creates a BeautifulSoup object that finds each subject block,
+        returns an array of JSON Objects containing Name, Instructor, Attendance & Link
+        to that particular subject
+
+        Parameters:
+            html (str): string object containing the response content (html, inline-css & inline-js code)
+
+        Returns:
+            subjects (list[dict]): List of dictionary object containing the subject details:\n
+                \t- Name
+                \t- Instructor
+                \t- Attendance
+                \t- Link
+                \t- Course ID
+    """
+
+    soup = BeautifulSoup(html, 'lxml')
+    subjects = []
+
+    subject_containers = soup.find_all(class_="subcontent-container")
+    # print(len(subject_containers))
+
+    for subject in subject_containers:
+        #convert to raw string, so it can be parsed after being converted to BS4
+        subject = str(subject)
+        soup = BeautifulSoup(subject, 'lxml')
+
+        #Name
+
+        subject_name = soup.find('h4')
+        subject_name = subject_name.contents[0] # getting the data within the actual tag that stored the name
+
+        #Instructor
+        subject_instructor = soup.select_one('.istruinfocontainer > div') # probably meant to write "instructor-info-container" but you never know 💀
+        subject_instructor = subject_instructor.contents[0]
+
+        #Subject Attendance
+        subject_attendance = soup.select_one('.prg-container > span') #progress container is the weird horizontal graph thingy
+        subject_attendance = subject_attendance.contents[0] if subject_attendance != None else "0%"
+
+        #Subject links
+        subject_link = soup.find('a')
+        subject_link = subject_link['href']
+
+        #Course id
+        course_id = subject_link[-4:]
+
+        subject_dict = {
+            'name':subject_name,
+            'instructor':subject_instructor,
+            'attendance':subject_attendance,
+            'link':subject_link,
+            'course_id':course_id
+        }
+
+        subjects.append(subject_dict)
+
+    return subjects
+
+
+def parse_materials(html : str) -> list[dict]:
+    """
+        Similar to parse_subjects, it gets all the "activity" within a subjects, extracts a name and link to that subject, and
+        return an array of JSON Objects containing these properties.
+
+        Parameters:
+            html (str): string object containing the response content (html, inline-css & inline-js code)
+
+        Returns:
+            course_materials (list[dict]): List of dict objects containing name and link to each course material available for given subject 
+    """
+
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    activities = soup.select(".activityinstance")
+
+    # SELECTING all anchor tags, seems to give me access to past semester and all their contents, might be useful
+    # Something to look into for the future.
+
+    course_materials = []
+    for activity in activities:
+        soup = BeautifulSoup(str(activity), 'lxml')
+        link = soup.select_one("a")
+        link = link['href']
+        # print(link)
+
+        name = soup.find("span")
+        name = name.contents[0]
+        link_type = link[34:-19] # 34 and -19 are just constants to remove the unecessary part of the link
+
+        obj = soup.select(".accesshide")
+        doc_type = "unknown"
+        if(len(obj)):
+            doc_type = obj[0].contents[0]
+        # else:
+            # print(obj)
+        #The handling for the different potential scenarios regarding the way the download links have to be made available is 
+        # to be seperated from this request.
+        activity_object = {
+            'name':name,
+            'link':link,
+            'type':link_type,
+            'doctype':doc_type
+        }
+
+        course_materials.append(activity_object)
+
+
+    return course_materials
+
+
+def parse_resource_link(html: str, link_type: str) -> str:
+    """
+        Given a link to a resource, dyquestion, presentation etc, returns a downloadable link to the content
+
+        Parameters:
+            html (str) : raw response data that is to be processes
+            link_type (str) : type of the resource [dyquestion, presentation, page, etc]
+    """
+
+
+    # Currently only supporting a subset of all types, but the most frequently used.
+    # It's not known whether there can be multiple links uploaded within an activity instance in the official site.
+
+    # extract the type of link (dyquestion, forum, flexpaper, etc etc)
+
+    soup = BeautifulSoup(html, 'lxml')
+
+
+
+    # This method extracts the link from the javascript code that loads the actual document
+    def flexpaper_parse(soup):
+        #find the script that handles "displaying the pdf file within the custom (flexpaper) pdf viewer"
+        script_tag = soup.find('script', string=re.compile(r"\$\(.*?\).FlexPaperViewer"))
+        if script_tag != None:
+            #Extract the PDFFILE link from within its config by means of regex
+            pdf_url_pattern = re.compile(r"PDFFile\s*:\s*'([^']+)'")
+            pdf_url_match = pdf_url_pattern.search(script_tag.string)
+
+            if(pdf_url_match):
+                #successfully found the PDFFILE url
+                pdf_url = pdf_url_match.group(1)
+                # print("PDF file URL:", pdf_url)
+                return pdf_url
+                # print(pdf_url_match)
+
+            else:
+                logging.debug("Did not find the link within flexpaper config")
+                return None
+
+    #This method is slightly slower as it extracts after loading a new page.
+    def default_parse(soup):
+        presentation_content = soup.find('div', attrs={'role':'main'})
+
+        soup = BeautifulSoup(str(presentation_content), 'lxml')
+        link_to_content = soup.find("a")
+        link_to_content = link_to_content['href']
+
+        # print("FIRST: ",link_to_content) 
+        return link_to_content
+
+
+    match link_type:
+
+        case "flexpaper":
+            return flexpaper_parse(soup)
+
+        case "resource":
+            return default_parse(soup)
+
+        case "presentation":
+            return default_parse(soup)
+
+        case "dyquestion":
+            return default_parse(soup)
+
+        case "questionpaper":
+            return default_parse(soup)
+
+        case "forum":
+            # Currently I have no plans on supporting a forum via this API
+            return None
+
+        case "quiz":
+            #  quizzes are also not being supported by this API
+            return None
+
+        case _:
+            return None
+
+
+
+
+def parse_attendance(html: str) -> list[dict]:
+    """
+      Takes in the HTML response object, and returns a list of JSON Subject Objects with the following data:
+
+      - subject : subject name
+      - total : total no. of classes
+      - present : no. of classes attended
+      - absent : no. of classes missed
+      - percentage: ratio of present/absent 
+
+    """ 
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    subjects = soup.select('.cell')
+
+    i = 0
+    overall_attendance_data = []
+    subject_attendance_data = {} 
+    for column in subjects: 
+        value = column.get_text(strip=True) 
+        # print(value)
+        choice = i%5
+        if value == '--':
+            value = 0
+
+        match choice:
+            case 0:
+                subject_attendance_data['subject'] = str(value) 
+            case 1:
+                subject_attendance_data['total'] = str(value)
+            case 2:
+                subject_attendance_data['present'] = str(value)
+            case 3: 
+                subject_attendance_data['absent'] = str(value)
+            case 4:
+                # This indicates end of row, so add the collected data to the overall data 
+                subject_attendance_data['percentage'] = str(value)
+
+                overall_attendance_data.append(subject_attendance_data)
+                subject_attendance_data = {}
+        i = ( i + 1 ) % 5
+
+
+    return overall_attendance_data  
+
+
+
+
